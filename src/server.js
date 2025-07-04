ca48baa1561d460c98a98900d63e9fbf81a1caf2
jvs-dev <shepherdcom12@gmail.com>
2025-07-04 01:18:39 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+// src/server.js
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); // Carrega as variáveis de ambiente
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors()); // Permite requisições de outras origens (seu frontend)
+app.use(express.json()); // Habilita o parsing de JSON no corpo das requisições
+
+app.use('/api', routes); // Usa as rotas definidas em routes.js com prefixo /api
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
